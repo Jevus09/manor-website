3adb91b6127618c14000d41ab19e071f384bee04
jevus09 <jevus09@gmail.com>
2023-06-14 11:43:12 -0500

    continued navbar. initiated carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import HeaderNavbar from './components/Navbar';
+import HeaderNavbar from './components/navbar/TopNav';
+import BottomNav from './components/navbar/BottomNav';
 import './index.css';
+import FrontCarousel from './components/FrontCarousel';
 
 
 function App() {
   return (
     <div className="App">
       <HeaderNavbar/>
+      <BottomNav/>
+      <FrontCarousel/>
     </div>
   );
 }
