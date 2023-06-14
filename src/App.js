ede3ee1a7f21119ca5d67b4a70d84d71f505eded
jevus09 <jevus09@gmail.com>
2023-06-13 20:30:24 -0500

    Initiated project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import HeaderNavbar from './components/Navbar';
+import './index.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderNavbar/>
     </div>
   );
 }
