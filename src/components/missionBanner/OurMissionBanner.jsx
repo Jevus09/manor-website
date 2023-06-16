ea54febb85ad69d41ba3c55b135f219d292fd307
LuisCldrn <Luis.cldrn@yahoo.com>
2023-06-16 13:44:56 -0500

    updated mission banner

--- src/components/missionBanner/OurMissionBanner.jsx
+++ src/components/missionBanner/OurMissionBanner.jsx
@@ -6,12 +6,13 @@ const OurMissionBanner = () => {
 
   return (
     <div className='main' >
-        <img style={{height: '30vh'}} src={missionImg} alt="" />
+        <img className='missionImg' style={{height: '30vh'}} src={missionImg} alt="" />
         <div className="text">
           <div className='textarea'>
-            <h1>Our Mission</h1>
+            <h1 className='missionText'>'' Our Mission</h1>
             <p>
-            Unlike a traditional nursing home, our services extend to those requiring a short term stay in our Sub-acute program. With newly designed private rooms, we cater to the needs of patients who may require 24 hour skilled nursing care as well as intensive therapy services. Lee Manor is proud to offer a superb, well rounded array of services and activities to enhance the lives of our residents.</p>
+           We start by making the patient assessment and follow through. Directing the Home Care Program every step of the way. </p>
+           - John Smith, RN
           </div>
         </div>
       </div>
