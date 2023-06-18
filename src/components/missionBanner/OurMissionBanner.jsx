bd11a8e044c179f584ce5fe4032707f4d77e39d3
jevus09 <jevus09@gmail.com>
2023-06-18 12:12:31 -0500

    fixed bug with OurMissionBanner background

--- src/components/missionBanner/OurMissionBanner.jsx
+++ src/components/missionBanner/OurMissionBanner.jsx
@@ -5,10 +5,10 @@ import './missionBanner.css'
 const OurMissionBanner = () => {
 
   return (
-    <div className='main h-80' >
+    <div className='main h-80 ' >
         <img className='missionImg' style={{height: '30vh'}} src={missionImg} alt="" />
         <div className="text">
-          <div className='textarea'>
+          <div className='textarea bg-white'>
             <h1 className='missionText'>'' Our Mission</h1>
             <p>
            We start by making the patient assessment and follow through. Directing the Home Care Program every step of the way. </p>
