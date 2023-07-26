3aeebea551b6bec13f689aba6bbe75cefbe8c166
jevus09 <jevus09@gmail.com>
2023-07-26 06:49:28 -0500

    updated programs component

--- src/components/Programs.js
+++ src/components/Programs.js
@@ -1,65 +1,47 @@
 import React from 'react';
-import { Card, ListGroup } from 'react-bootstrap';
-import HOSPICE from '../assets/programs/hospice.webp'
+import { Card } from 'react-bootstrap';
+import PULMO from '../assets/programs/pulmonary.webp'
 import IV from '../assets/programs/ivtherapy.webp'
 import ORTHO from '../assets/programs/ortho.webp'
 import UROLOGY from '../assets/programs/uro.webp'
 import WOUND from '../assets/programs/wound.webp'
 import NEUROLOGY from '../assets/programs/neuro.webp'
-import { TbTilde } from 'react-icons/tb';
+
 
 
 const data = [
     {
         id: 1,
         title: 'NEUROLOGY PROGRAM',
-        point1: 'Neurologists consult on-site monthly',
-        point2: ' Stroke rehab program',
-        point3: 'Neurologists consult on-site monthly. Neurologists consult on-site monthly',
         background: NEUROLOGY,
     },
     {
         id: 2,
         title: 'WOUND CARE PROGRAM',
-        point1: 'Wound Care Certified Registered Nurse on staff',
-        point2: ' Weekly wound rounds with a board certified surgeon',
-        point3: 'Wound Vac, Mist therapy, specialty beds, edema wrapping, and other modalities',
         background: WOUND,
     },
 
     {
         id: 3,
         title: 'UROLOGY PROGRAM',
-        point1: 'Provides comprehensive inpatient and outpatient care, including state-of-the-art diagnostic and treatment capabilities.',
-        point2: 'Urologist that rounds on-site monthly',
-        point3: 'Bladder Scanner',
         background: UROLOGY,
     },
 
     {
         id: 4,
-        title: 'HOSPICE PROGRAM',
-        point1: 'Contracted inpatient saervices with Advocate Hospice',
-        point2: ' Contracted with multiple Hospice providers in the area',
-        point3: 'Contracted with multiple Hospice providers in the area',
-        background: HOSPICE ,
+        title: 'PULMONARY REHAB',
+        background: PULMO ,
     },
 
     {
         id: 5,
         title: 'IV THERAPY',
-        point1: 'IV certified nursing staff.',
-        point2: ' TPN',
-        point3: 'IV therapy is widely recommended for patients in rehab because it is an extremely safe, relatively non-invasive',
         background: IV ,
     },
 
     {
         id: 6,
         title: 'ORTHO PROGRAM',
-        point1: 'On site Physical and Occupational therapists 7 days a week',
-        point2: ' We follow individual surgeon protocols',
-        point3: 'Comprehensive pain management. INR machine',
         background: ORTHO ,
     },
 
@@ -73,27 +55,20 @@ const Programs = () => {
       <div className="grid grid-cols-2 lg:grid-cols-3 justify-items-center 2xl:px-72 gap-4">
         {data.map((item) => (
           <div key={item.id} className="place-content-center w-max">
-            <a href='/#' className='no-underline'>
-              <Card className="group program-card" style={{ height: '16rem', width: '18rem' }}>
+            <div >
+              <Card className="program-card" style={{ height: '16rem', width: '18rem' }}>
                 {/* Cover of Card */}
                 <Card.Body
-                  className="bg-center bg-cover flex flex-col justify-center items-center md:group-hover:hidden"
+                  className="bg-center bg-cover flex flex-col justify-center items-center"
                   style={{ backgroundImage: `linear-gradient(#00000085, #00000085), url(${item.background})` }}
                 >
                   <Card.Text>
                     <p className="text-sm md:text-base text-center font-bold text-white">{item.title}</p>
                   </Card.Text>
                 </Card.Body>
-                {/* Inside of Card */}
-                <Card.Body  className="hidden md:group-hover:block bg-[#3f87b9]">
-                  <Card.Text className='inside-text flex flex-col items-center justify-center text-center font-bold text-white'>
-                    <ListGroup.Item className='mt-4'>Lorem ipsum dolor sit amet consectetur</ListGroup.Item>
-                    <TbTilde className='text-center text-xl my-2' />
-                    <ListGroup.Item className='text-sm text-[#98bad6] font-thin'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</ListGroup.Item>
-                  </Card.Text>
-                </Card.Body>
+            
               </Card>
-            </a>
+            </div>
           </div>
         ))}
       </div>
