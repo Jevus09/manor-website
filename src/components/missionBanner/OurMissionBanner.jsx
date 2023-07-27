47ba3fa3784365a16c6e6830b7577b02c5c5aa7c
jevus09 <jevus09@gmail.com>
2023-07-27 18:19:33 -0500

    a

--- src/components/missionBanner/OurMissionBanner.jsx
+++ src/components/missionBanner/OurMissionBanner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import missionImg from '../../assets/mission.webp';
-import './missionBanner.css'
+
 
 const OurMissionBanner = () => {
 
@@ -9,8 +9,8 @@ const OurMissionBanner = () => {
       <div className="flex justify-center items-center h-full w-screen md:w-auto">
         <img className='hidden sm:block h-full ' src={missionImg} alt="" />
         <div className='flex flex-col h-max-full  '>
-          <h1 className='missionText text-7xl'>'' Our Mission</h1>
-          <p className=' w-96 text-sm text-start px-4 '>
+          <h1 className='missionText'>'' Our Mission</h1>
+          <p className=' missiongp w-96 text-sm text-start px-4 '>
             Lee Manor is committed to providing services which promote the highest standards of well-being in a safe, comfortable, community-integrated environment catered to the unique needs of our residents while promoting each resident's dignity and individuality.
           </p>
         </div>
