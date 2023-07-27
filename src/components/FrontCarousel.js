6fb4452038dd3897924943056e7acfde4b086d39
jevus09 <jevus09@gmail.com>
2023-07-27 18:12:28 -0500

    updated font size for title

--- src/components/FrontCarousel.js
+++ src/components/FrontCarousel.js
@@ -13,7 +13,7 @@ function FrontCarousel() {
           <div className="d-block w-100 h-128 brightness-75 carimage" style={{ backgroundImage: `url(${first})` }}></div>
           <div className="">
             <Carousel.Caption className="h-72 ">
-              <h1 className="text-3xl md:text-6xl italic  " style={{ maxHeight: '4vh' }}>Our Family to Yours</h1>
+              <h1 className="text-3xl sm:text-6xl italic  " style={{ maxHeight: '4vh' }}>Our Family to Yours</h1>
               <p className="my-8 font-light hidden sm:block ">Lee Manor employs only the most qualified specialists, <br /> licensed nurses, and certified nursing assistants.<br /> Our staff will give you the peace of mind you deserve during <br /> your loved one's stay at Lee Manor.</p>
               <a href='/contact' className=" border border-line px-3 py-2 rounded-full text-white no-underline hidden sm:inline-block">Get in Touch</a>
             </Carousel.Caption>
@@ -24,7 +24,7 @@ function FrontCarousel() {
         <Carousel.Item >
           <div className="d-block w-100 h-128 brightness-75 carimage" style={{ backgroundImage: `url(${second})` }}  ></div>
           <Carousel.Caption >
-            <h1 className=" text-3xl md:text-6xl " style={{ maxHeight: '4vh' }} >Therapy You Can Trust</h1>
+            <h1 className=" text-3xl sm:text-6xl " style={{ maxHeight: '4vh' }} >Therapy You Can Trust</h1>
             <p className="my-8 font-light hidden sm:block">Ability Rehab is a comprehensive rehabilitative contract <br /> provider of Physical, Occupational and Speech-Language <br /> therapy services. </p>
             <a href='/#' className=" border border-line px-3 py-2 rounded-full text-white no-underline hidden sm:inline-block ">Learn More</a>
           </Carousel.Caption>
@@ -34,7 +34,7 @@ function FrontCarousel() {
         <Carousel.Item >
           <div className="d-block w-100 h-128 brightness-75 carimage" style={{ backgroundImage: `url(${third})` }}></div>
           <Carousel.Caption >
-            <h1 className="text-3xl md:text-6xl" style={{ maxHeight: '10vh' }}>Excellence in Care</h1>
+            <h1 className="text-3xl sm:text-6xl" style={{ maxHeight: '10vh' }}>Excellence in Care</h1>
             <p className="my-8 font-light hidden sm:block">Lee Manor is proud to offer a superb, well rounded array of services and activities to enhance the lives of our residents.</p>
 
           </Carousel.Caption>
