1ffcca9641c90252de84db71c994cd7f8893c6de
jevus09 <jevus09@gmail.com>
2023-09-02 19:35:09 -0500

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,18 +6,18 @@ const Footer = () => {
             <footer className="footer p-10 bg-base-300 text-base-content footer-test flex justify-center gap-20 md:gap-60 items-center">
             <div>
                     <span className="footer-title text-white">Company</span>
-                    <a className="link link-hover text-[#5e636c]" href='/'>Home</a>
-                    <a className="link link-hover text-[#5e636c]" href='/about'>About us</a>
-                    <a className="link link-hover text-[#5e636c]" href='/contact'>Contact</a>
-                    <a className="link link-hover text-[#5e636c]" href='https://www.indeed.com/cmp/Meadowbrook-Bolingbrook/jobs/l-Des-Plaines,-IL' rel="noreferrer" target='_blank' >Jobs</a>
+                    <a className="link link-hover text-white" href='/'>Home</a>
+                    <a className="link link-hover text-white" href='/about'>About us</a>
+                    <a className="link link-hover text-white" href='/contact'>Contact</a>
+                    <a className="link link-hover text-white" href='https://www.indeed.com/cmp/Meadowbrook-Bolingbrook/jobs/l-Des-Plaines,-IL' rel="noreferrer" target='_blank' >Jobs</a>
 
                 </div>
-                <div>
+                <div className=''>
                     <span className="footer-title text-white">Services</span>
-                    <a className="link link-hover text-[#5e636c]" href='/rehab-programs'>Rehabilitation</a>
-                    <a className="link link-hover text-[#5e636c]" href='/additional-services'>Additional Services</a>
-                    <a className="link link-hover text-[#5e636c]" href='/nursing-care'>Skilled Nursing Care</a>
-                    <a className="link link-hover text-[#5e636c]" href='/privacy-policy'>Privacy Policy</a>
+                    <a className="link link-hover text-white" href='/rehab-programs'>Rehabilitation</a>
+                    <a className="link link-hover text-white" href='/additional-services'>Additional Services</a>
+                    <a className="link link-hover text-white" href='/nursing-care'>Skilled Nursing Care</a>
+                    <a className="link link-hover text-white" href='/privacy-policy'>Privacy Policy</a>
                 </div>
             </footer>
             <div className='flex bg-[#15191e] justify-center text-white text-xs '>
